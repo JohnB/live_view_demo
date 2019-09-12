1fe87c925fdb1fe2752ab14117a2bd237b887ad3
JohnB <john.baylor@gmail.com>
2019-09-11 23:08:37 -0700

    CargoCult debugging.

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_view_demo, LiveViewDemoWeb.Endpoint,
-  url: [host: "https://shrouded-spire-77484.herokuapp.com/", port: 443, scheme: "https"],
+  url: [host: "https://shrouded-spire-77484.herokuapp.com", port: 443, scheme: "https"],
 #  url: [host: "https://rocky-spire-88257.herokuapp.com/", port: 443, scheme: "https"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
