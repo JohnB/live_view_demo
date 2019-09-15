c6ff46a50b4c00002f978e1afd6391cf98d18614
JohnB <john.baylor@gmail.com>
2019-09-15 12:32:02 -0700

    Randomize the start squares; mix up colors.

--- lib/live_view_demo/board.ex
+++ lib/live_view_demo/board.ex
@@ -8,14 +8,22 @@ defmodule Board do
   defstruct [:board_squares, :width, :height, :start_squares, :pieces]
 
   def new( width \\ default_width(), height \\ default_height(), _start_style \\ :corners) do
+    # Randomize our start squares w/in the corner quadrant
+    top_left_x = Enum.random([0, 1, 2, 3])
+    top_left_y = Enum.random([0, 1, 2, 3])
+    top_left = width * top_left_y + top_left_x
+    top_right = width * (top_left_x + 1) - top_left_y - 1
+    bot_right = width * height - top_left - 1
+    bot_left = width * height - top_right - 1
+    
     %__MODULE__{
       width: width,
       height: height,
       start_squares: %{
-        top_left_index(width, height) => "red",\
-        top_right_index(width, height) => "yellow",
-        bottom_right_index(width, height) => "green",
-        bottom_left_index(width, height) => "blue"
+        top_left => "blue",
+        top_right => "green",
+        bot_right => "yellow",
+        bot_left => "red"
       },
       board_squares:
         Enum.reduce( 1..(width * height), %{},
@@ -38,10 +46,6 @@ defmodule Board do
   def default_height() do
     Enum.random([18,19,21,22])
   end
-  def top_left_index(_width, _height) do 0 end
-  def top_right_index(width, _height) do width - 1 end
-  def bottom_right_index(width, height) do width * height - 1 end
-  def bottom_left_index(width, height) do width * (height - 1) end
   
   # Return the CSS class(es) that should be applied to this square.
   def square_class(%__MODULE__{start_squares: start_squares, board_squares: board_squares}, square_index) do
